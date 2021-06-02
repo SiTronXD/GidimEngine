c2b36102f98eb4e72379e9d176be766d33bea9d7
Bensson <47039664+SiTronXD@users.noreply.github.com>
2021-06-02 13:58:15 +0200

    Showcase images

--- Gidim/Resources/Shaders/PixelShaders/WaterShader_Pix.hlsl
+++ Gidim/Resources/Shaders/PixelShaders/WaterShader_Pix.hlsl
@@ -54,7 +54,7 @@ float calcFresnel(float3 viewDir, float3 normal)
 
 float calcSpecular(float3 sunDir, float3 normal, float3 viewDir, float camToPosDist)
 {
-	const float specGloss = 96.0;
+	const float specGloss = 128.0;
 	const float specIntensity = 0.75;
 
 	// Reflect
